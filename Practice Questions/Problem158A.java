93cf95bd4d44f990eeee19892ac6c382443052bb
Ayushi Gupta <ayushig705@gmail.com>
2021-08-01 13:00:09 -1200

    updated problem soln

---
+++ Practice Questions/Problem158A.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Problem158A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int count = 0;
+        int[] p = new int[n];
+        for (int i = 0; i < n; i++) {
+            p[i] = sc.nextInt();
+        }
+        for (int j = 0; j < n; j++) {
+            if (p[k - 1] <= p[j] && (p[k - 1] != 0 || p[j] != 0)) {
+                count++;
+            } else
+                break;
+        }
+        System.out.println(count);
+    }
+}
