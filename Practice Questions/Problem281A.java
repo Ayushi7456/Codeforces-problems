2c36b48d86c2c2017a0a02ed554cad19a2e44e73
Ayushi Gupta <ayushig705@gmail.com>
2021-08-04 17:15:43 -1200

    updated soln

---
+++ Practice Questions/Problem281A.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Problem281A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.next();
+        String firstLetter = word.substring(0, 1);
+        String remainingLetters = word.substring(1, word.length());
+        firstLetter = firstLetter.toUpperCase();
+        word = firstLetter + remainingLetters;
+        System.out.println(word);
+    }
+}
