985aa400bc6268435a1069d42ee07eb33f7e4dbe
Ayushi Gupta <ayushig705@gmail.com>
2021-08-07 23:16:56 -1200

    updated soln

---
+++ Practice Questions/Hulk705A.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+public class Problem705A{
+                 public static void main(String[]args){
+                                 Scanner sc = new Scanner(System.in);
+                                 int n = sc.nextInt();
+                                 String oddFeeling = "I hate ";
+                                 String evenFeeling = "I love ";
+                                 String middleWord = "that ";
+                                 String endWord = "it";
+                                 String result = "";
+                                 for(int i=1; i<=n; i++){
+                                           if(i%2==0){
+                                                result = result + evenFeeling;
+                                           }   
+                                           else{
+                                                result = result + oddFeeling;            
+                                           }
+                                           if(i==n){
+                                                result = result + endWord;
+                                           }
+                                           else{
+                                                result = result + middleWord;
+                                           }
+                                 }
+                                 System.out.println(result);
+                                 
+                                 
+                 }
+}
